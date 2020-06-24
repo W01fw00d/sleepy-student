0b41f1ee3458af3b9fe5f719de4032383115b5d4
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-06-24 19:00:45 +0900

    Implement daily class room link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import copy from "copy-to-clipboard";
+
+import classesSchedule from "./schedules/level1.json";
 
 function App() {
+  const ROOM_BASE_URL = "https://zoom.us/j/";
+  var DAYS = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const dayName = DAYS[new Date().getDay()];
+  const todaysClass = classesSchedule[dayName];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" onClick={() => copy(todaysClass.password)}>
+        {todaysClass ? (
+          <>
+            <p>
+              Good morning! Today you will attend {todaysClass.class}, with{" "}
+              {todaysClass.teacher}
+              -sensei
+            </p>
+            <a
+              href={`${ROOM_BASE_URL}${todaysClass.roomId}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go to class!
+            </a>
+          </>
+        ) : (
+          <p> You have no classes scheduled for today </p>
+        )}
       </header>
     </div>
   );
